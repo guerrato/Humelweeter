a7aa1b76b9c95ddaff39b383d45f16551d14c104
Pedro Guerrato <pguerrato@gmail.com>
2018-05-27 23:23:43 -0300

    Finished index.html and started result.html

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let fs = require('fs')
 
 let pathView = __dirname + '/views/';
 
+function getToday() {
+	let now = new Date()
+	let mm = now.getMonth() + 1
+	let dd = now.getDate()
+
+	return [now.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('-')
+}
+
 app.get('/', (req, res) => {
 	res.sendFile(pathView + 'index.html')
 })
@@ -13,18 +21,20 @@ app.get('/result', (req, res) => {
 	res.sendFile(pathView + 'result.html')
 })
 
-app.use("*", (req,res) => {
-  res.sendFile(pathView + "index.html");
-});
+app.use("*", (req, res) => {
+	res.sendFile(pathView + "index.html")
+})
 
 io.on('connection', (socket) => {
-	console.log('a user connected');
-	
+	socket.join('humelroom')
+	console.log('a user connected')
+
 	socket.on('disconnect', () => {
-    console.log('user disconnected')
+		console.log('user disconnected')
 	})
-	
+
 	socket.on('tweeted', () => {
+		console.log('tweeted')
 		let obj = {}
 
 		fs.readFile('db.json', 'utf8', function (err, data) {
@@ -32,35 +42,36 @@ io.on('connection', (socket) => {
 				throw err
 			}
 
-			obj = JSON.parse(data)
+			if (data != "") {
+				obj = JSON.parse(data)
+			}
 
 			let now = new Date()
 			let mm = now.getMonth() + 1
 			let dd = now.getDate()
 
-			let date = [now.getFullYear(), (mm>9 ? '' : '0') + mm, (dd>9 ? '' : '0') + dd].join('-')
-				 
-			if(date in obj) {
+			let date = [now.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('-')
+
+			if (date in obj) {
 				obj[date]['tweet'] += 1
 			} else {
-				obj = {
-					[date]: {
-						'tweet': 1,
-						'humification': 0
-					} 
+				obj[date] = {
+					'tweet': 1,
+					'humification': 0
 				}
 			}
 
 			fs.writeFile('db.json', JSON.stringify(obj), (err) => {
-				if(err) {
+				if (err) {
 					throw err
 				}
 			})
-
 		})
-  })
-	
+	})
+
 	socket.on('humified', () => {
+		console.log('humified')
+		
 		let obj = {}
 
 		fs.readFile('db.json', 'utf8', function (err, data) {
@@ -68,37 +79,34 @@ io.on('connection', (socket) => {
 				throw err
 			}
 
-			obj = JSON.parse(data)
+			if (data != "") {
+				obj = JSON.parse(data)
+			}
 
 			let now = new Date()
 			let mm = now.getMonth() + 1
 			let dd = now.getDate()
 
-			let date = [now.getFullYear(), (mm>9 ? '' : '0') + mm, (dd>9 ? '' : '0') + dd].join('-')
-				 
-			if(date in obj) {
+			let date = [now.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('-')
+
+			if (date in obj) {
 				obj[date]['humification'] += 1
 			} else {
-				obj = {
-					[date]: {
-						'tweet': 0,
-						'humification': 1
-					} 
+				obj[date] = {
+					'tweet': 0,
+					'humification': 1
 				}
 			}
 
-			fs.writeFile('db.json', JSON.stringify(obj), (err) => {
-				if(err) {
+			fs.writeFile('db.json', JSON.stringify(obj), (err, socket) => {
+				if (err) {
 					throw err
 				}
-		
-				console.log("The file was saved!");
 			})
-
-		})
-  })
+		})	
+	})
 })
 
 http.listen(3000, () => {
-	console.log('listening on *:3000');
+	console.log('listening on *:3000')
 })
